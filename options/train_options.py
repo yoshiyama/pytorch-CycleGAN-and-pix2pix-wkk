1f13a39d39f2b77ed3adde195971087f95ea326f
yoshiyama <yamamoto.ait@gmail.com>
2023-06-21 20:20:01 +0900

    changes becase

--- options/train_options.py
+++ options/train_options.py
@@ -35,6 +35,8 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
+        parser.add_argument('--mask_weight', type=float, default=1.0, help='weight for the mask, default is 1.0')
+
 
         self.isTrain = True
         return parser
